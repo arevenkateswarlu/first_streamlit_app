e9385aae93061f07141f0780784aea54241aa826
arevenkateswarlu <140911683+arevenkateswarlu@users.noreply.github.com>
2023-07-29 14:16:30 -0400

    Update streamlit_app.py
    Fixed the content

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,6 +2,6 @@ import streamlit
 
 streamlit.title('My parents New Healthy diner')
 streamlit.header('Breakfast Menu')
-streamlit.text('Omega 3 & Blueberry Oatmeal ')
-streamlit.text('Rice & Pappu ')
-streamlit.text('Egg , Chapathi and Thal ')
+streamlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.text('Kale, Spinach & Rocket Smoothie')
+streamlit.text('Hard-Boiled Free-Range Egg')
