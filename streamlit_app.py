9d5a98341a0c32c3c9fadcde481227a353957b49
arevenkateswarlu <140911683+arevenkateswarlu@users.noreply.github.com>
2023-07-29 14:14:58 -0400

    Update streamlit_app.py
    Typo fix

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,7 @@
 import streamlit
 
 streamlit.title('My parents New Healthy diner')
-steamlit.header('Breakfast Menu')
+streamlit.header('Breakfast Menu')
 streamlit.text('Omega 3 & Blueberry Oatmeal ')
 streamlit.text('Rice & Pappu ')
 streamlit.text('Egg , Chapathi and Thal ')
